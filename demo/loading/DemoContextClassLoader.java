1a09cb7c1253edccf0ba77b260e75b480cce796f
BoulCheng <1241292849@qq.com>
2019-03-28 21:27:43 +0800

    DemoContextClassLoader

---
+++ demo/loading/DemoContextClassLoader.java
@@ -0,0 +1,73 @@
+package loading;
+
+import java.sql.*;
+
+/**
+ * @author Yuanming Tao
+ * Created on 2019/3/20
+ * Description 线程上下文加载器
+ */
+public class DemoContextClassLoader {
+    // JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/zlb";
+
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "root";
+    static final String PASS = "1234";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            // 注册 JDBC 驱动
+            //A call to forName("X") causes the class named X to be initialized.
+            Class.forName("com.mysql.jdbc.Driver");
+
+            // 打开链接
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT id, gmt_create FROM db_test";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // 展开结果集数据库
+            while(rs.next()){
+                // 通过字段检索
+                int id  = rs.getInt("id");
+                String gmt_create = rs.getString("gmt_create");
+
+                // 输出数据
+                System.out.print("ID: " + id);
+                System.out.print(", gmt_create: " + gmt_create);
+                System.out.print("\n");
+            }
+            // 完成后关闭
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
+    }
+}
